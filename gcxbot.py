81f27adc97171158fce87fcfada85487f2cf4b77
BORWEY, JOSHUA <joshua.borwey@gmail.com>
2018-01-24 15:06:38 -0600

    working commit fixed config issues

--- gcxbot.py
+++ gcxbot.py
@@ -1,49 +1,32 @@
+from bs4 import BeautifulSoup
+
 import praw
+import time
+import re
 import requests
-from lxml import html
+import bs4
 
 path = '.\commented.txt'
-REDIRECT_URI = 'http://localhost:65010/reddit_callback'
-url = 'https://www.reddit.com/r/giftcardexchange/'
 
 
 def authenticate():
     print('Authenticating...\n')
-    reddit = praw.Reddit('explainbot', user_agent='web:subway-bot (by /u/ArtigoQ)')
+    reddit = praw.Reddit('subwaybot', user_agent = 'web:subwaybot:v0.1 (by /u/ArtigoQ)')
     print('Authenticated as {}\n'.format(reddit.user.me()))
     return reddit
 
 
 def run_subwaybot(reddit):
 
+    print("Getting 20 posts...\n")
 
-def get_parsed_page(url):
-    """return the content of the site page on the url in
-    a parsed lxml format that is easy to query."""
+    subreddit = reddit.subreddit('giftcardexchange')
+    for submission in subreddit.new(limit=30):
+        match = re.findall("Amazon", submission.title)
+        if match:
+            print('Match found in submission: ' + submission.title)
 
-    response = requests.get(url)
-    parsed_page = html.fromstring(response.content)
-    return parsed_page
-
-
-parsed_page = get_parsed_page(url)
-
-# Print the subreddits post titles
-parsed_page.xpath('//p/a/text()')  # ['Data, what now?'].
-
-
-post_urls = parsed_page.xpath('//p/a/@href')
-
-
-for post_url in post_urls:
-    print('Post url:', post_url)
-
-
-    parsed_post_page = get_parsed_page(post_url)
-    paragraph_title_xpath = '//p[@class="title"]/a/text()'
-    paragraph_titles = parsed_post_page.xpath(paragraph_title_xpath)
-    paragraph_titles = map(lambda x: ' \n ' + x, paragraph_titles)
-    print(''.join(paragraph_titles) + '\n')
+        time.sleep(10)
 
 
 def main():
